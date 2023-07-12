6de9fe63b2a5bb525e004c38826a89fd25cbf462
BhonePyae-Kyaw <bhonepyaekyaw99@gmail.com>
2023-07-13 02:01:06 +0700

    Finish all required features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,73 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [todoItem, setTodoItem] = useState("");
   const [todoList, setTodoList] = useState([]);
-  const [finishList, setFinishList] = useState([]);
-  const addItem = () => {
-    setTodoList(current => [...current, {todoItem : todoItem}]);
-    console.log(todoList)
+  const [checked] = useState(false);
+
+  useEffect(() => {
+    calculateFinishTask();
+  }, [todoList])
+
+  const calculateFinishTask = () => {
+    let taskCount = 0;
+    if (todoList.length > 1) {
+      for (let i in todoList) {
+        if (todoList[i].done) {
+          taskCount+= 1;
+        }
+      }
+    }
+    return (taskCount / todoList.length) * 100;
   }
+
+
+  //Adding item to the todo List
+  const addItem = () => {
+    setTodoList(current => [...current,
+      {
+        todoItem : todoItem,
+        done : checked
+      }
+    ]);
+  }
+
+  //Deleting item from the todo List
   const deleteTodo = (index) => {
     todoList.splice(index,1);
     setTodoList(() => ([...todoList]))
   }
-  const finishTask = (e) => {
-    console.log(e)
-    setFinishList(current => [...current, {itemFinished : e.todo.todoItem}]);
-    console.log('finish task')
-    console.log(finishList)
+
+  //Finish the task with check box
+  const finishTask = (index, done) => {
+    let temp = todoList[index];
+    temp.done = !done;
+    let todosClone = [...todoList];
+    todosClone[index] = temp;
+    setTodoList([...todosClone]);
   }
+
   return (
     <div className="App">
-      <h1>Hello World !</h1>
-      <input type='text' placeholder='Add a todo to your list:  ' onChange={(e) => setTodoItem(e.target.value)}/>
+      <h1>Hello World !</h1> 
+      <h4>{calculateFinishTask()} % done.</h4>
+      <input 
+        type='text' 
+        placeholder='Add a todo to your list:  ' 
+        onChange={(e) => setTodoItem(e.target.value)}
+      />
       <button onClick={addItem}>Add</button>
       <br />
       {todoList?.map((todo, index) => {
         return(
           <div key={index}>
-            <input type='checkbox' onClick={(e) => finishTask({todo})} />
-            <span>{index + 1} {todo.todoItem}</span>
+            <input 
+              type='checkbox' 
+              onChange={() => finishTask(index, todo.done)} 
+              checked={todo.done} 
+            />
+            <span className={`${todo.done ? 'checked' : ''}`}>{index + 1} {todo.todoItem}</span>
             <button onClick={() => deleteTodo(index)}>Delete</button>
           </div>
           
@@ -40,3 +78,4 @@ function App() {
 }
 
 export default App;
+
